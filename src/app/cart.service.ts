5bcd552a89d97766dc6eb407c9e4f97582185231
lilath <lilathviraja@gmail.com>
2019-07-05 15:59:52 +0530

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -4,7 +4,19 @@ import { Injectable } from '@angular/core';
    providedIn: 'root'}
 )
 export class CartService {
-
+  items = []
   constructor() { }
+
+  addItems(item){
+    this.items.push(item)
+  }
+
+  getitems(){
+    return this.items;
+  }
+  clearItems(){
+    this.items= []
+    return this.items
+  }
 
 }
